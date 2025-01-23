6728b273b758ef2da7188b4d3e2f77760054af97
Zhivko Shopov <zhishopov@gmail.com>
2025-01-23 19:13:07 +0000

    Fetching home page view

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import http from "http";
 
+import homeView from "./views/home/index.html.js";
+import siteCss from "./content/styles/site.css.js";
+
 const server = http.createServer((request, response) => {
   console.log("HTTP Request");
-  console.log(request.url);
 
-  response.write("Hello world!");
+  if (request.url === "/styles/site.css") {
+    response.writeHead(200, {
+      "content-type": "text/css",
+    });
+
+    response.write(siteCss);
+
+    return response.end();
+  }
+
+  response.write(homeView);
 
   response.end();
 });
